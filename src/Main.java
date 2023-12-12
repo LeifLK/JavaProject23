fa6599b88af955e09b6284adc8654689236e43eb
Leif <kruseleif@gmail.com>
2023-12-12 16:56:42 +0100

    functioning API.java and successfully tested in Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,57 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
+import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        API api = new API();
+        int testId = 59660;
+        int testId2 = 51;
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        try {
+            String droneDynamics = api.getDroneDynamics();
+            System.out.println("Drone Dynamics: " + droneDynamics);
+        } catch (IOException e) {
+            System.err.println("Error fetching Drone Dynamics: " + e.getMessage());
+            e.printStackTrace();
+        }
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+        try {
+            String droneDynamicsWithId = api.getDroneDynamics(testId);
+            System.out.println("Drone Dynamics with ID " + testId + ": " + droneDynamicsWithId);
+        } catch (IOException e) {
+            System.err.println("Error fetching Drone Dynamics with ID " + testId + ": " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        try {
+            String drones = api.getDrones();
+            System.out.println("Drones: " + drones);
+        } catch (IOException e) {
+            System.err.println("Error fetching Drones: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        try {
+            String dronesWithId = api.getDrones(testId2);
+            System.out.println("Drones with ID " + testId2 + ": " + dronesWithId);
+        } catch (IOException e) {
+            System.err.println("Error fetching Drones with ID " + testId2 + ": " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        try {
+            String droneTypes = api.getDroneTypes();
+            System.out.println("Drone Types: " + droneTypes);
+        } catch (IOException e) {
+            System.err.println("Error fetching Drone Types: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        try {
+            String droneTypesWithId = api.getDroneTypes(testId2);
+            System.out.println("Drone Types with ID " + testId2 + ": " + droneTypesWithId);
+        } catch (IOException e) {
+            System.err.println("Error fetching Drone Types with ID " + testId2 + ": " + e.getMessage());
+            e.printStackTrace();
         }
     }
 }
