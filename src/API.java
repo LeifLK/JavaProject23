0210f5914af77bb6109b8f8954668d5ba7bacad3
Leif <kruseleif@gmail.com>
2023-12-12 16:56:42 +0100

    functioning API.java and successfully tested in Main.java

--- src/API.java
+++ src/API.java
@@ -6,39 +6,92 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 
-    public class API {
+public class API {
+    private static final String BASE_URL = "https://dronesim.facets-labs.com/api/";
+    private static final String TOKEN = "Token 96abe845d26eafd5c6d920a152a52a5185b4bc24";
 
-        private static final String USER_AGENT = "";
-        private static final String ENDPOINT_URL = "https://dronesim.facets-labs.com/api/drones/?format=json";
-        private static final String TOKEN = "Token 96abe845d26eafd5c6d920a152a52a5185b4bc24";
+    //Nur eine gemacht zum testen, dann später get() für alle möglichkeiten (mit ohne id)
+    public String getDroneDynamics() throws IOException {
+        return ApiRequest("dronedynamics/?format=json");
+    }
 
-        public static void main(String[] args) {
-            System.out.println("Test started...");
+    public String getDroneDynamics(int id) throws IOException {
+        String endpoint = "dronedynamics/" + id + "/?format=json";
+        return ApiRequest(endpoint);
+    }
+
+    public String getDrones() throws IOException {
+        return ApiRequest("drones/?format=json");
+    }
+
+    public String getDrones(int id) throws IOException {
+        String endpoint = "drones/" + id + "/?format=json";
+        return ApiRequest(endpoint);
+    }
+
+    public String getDroneTypes() throws IOException {
+        return ApiRequest("dronetypes/?format=json");
+    }
+
+    public String getDroneTypes(int id) throws IOException {
+        String endpoint = "dronetypes/" + id + "/?format=json";
+        return ApiRequest(endpoint);
+    }
 
 
-            try {
-              URL url = new URL(ENDPOINT_URL);
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                connection.setRequestProperty("Authorization", TOKEN);
-                connection.setRequestMethod("GET");
-                connection.setRequestProperty("User-Agent", USER_AGENT); //kann man vllt weg lassen
-                int responseCode = connection.getResponseCode();
 
-                System.out.println("Response Code " + responseCode);
+
+    //Die funktion die den API request macht
+    private String ApiRequest(String endpoint) throws IOException {
+        URL url = new URL(BASE_URL + endpoint);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("Authorization", TOKEN);
+        connection.setConnectTimeout(5000); // 5 seconds
+        connection.setReadTimeout(5000); // 5 seconds
+
+        try {
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
                 BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
                 String inputLine;
-                StringBuffer response = new StringBuffer();
+                StringBuilder response = new StringBuilder();
                 while ((inputLine = in.readLine()) != null) {
                     response.append(inputLine);
                 }
                 in.close();
-                System.out.println(response.toString()); // Process your responseß
-            } catch (MalformedURLException e) {
-                System.err.println("Malformed URL: " + e.getLocalizedMessage());
-                e.printStackTrace();
-            } catch (IOException e) {
-                System.out.println("General IO Exception: " + e.getLocalizedMessage());
-                e.printStackTrace();
+                return response.toString();
+            } else {
+                // Handle non-200 responses
+                throw new IOException("Received non-OK response: " + responseCode);
             }
+        } finally {
+            connection.disconnect();
         }
+    }
+
+
+    //Test
+    public static void main(String[] args) {
+        API api = new API();
+
+        // Test the method without ID
+        try {
+            String resultWithoutId = api.getDroneDynamics();
+            System.out.println("Result without ID: " + resultWithoutId);
+        } catch (IOException e) {
+            System.err.println("An error occurred while fetching drone dynamics without ID: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        // Test the method with an ID, for example, ID = 1
+        try {
+            int testId = 59660; // You can change this ID to test different cases
+            String resultWithId = api.getDroneDynamics(testId);
+            System.out.println("Result with ID " + testId + ": " + resultWithId);
+        } catch (IOException e) {
+            System.err.println("An error occurred while fetching drone dynamics with ID: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
 }
