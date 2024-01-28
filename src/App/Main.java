1601828b704b4e175a3eeb6ee2a48ad0314f6f41
Leif <kruseleif@gmail.com>
2024-01-28 22:01:16 +0100

    changed Main

--- src/App/Main.java
+++ src/App/Main.java
@@ -1,13 +1,10 @@
 package App;
 
-import App.Model.DroneDynamics;
 import App.Services.DataStorage;
 import App.UI.LandingPage;
 
-import java.util.List;
-
 public class Main {
-    private static DataStorage dataStorage = new DataStorage();
+    private static final DataStorage dataStorage = new DataStorage();
 
     public static DataStorage getDataStorage()
     {
@@ -36,7 +33,6 @@ public class Main {
             System.out.println(dataStorage.getDroneTypeList().get(1).getManufacturer());
             System.out.println(dataStorage.getDroneTypeList().get(1).getTypename());
 */
-            List<DroneDynamics> drone71DynamicsList = dataStorage.getDynamicsForDrone(71);
 
             LandingPage landingPage = new LandingPage();
             landingPage.show();
