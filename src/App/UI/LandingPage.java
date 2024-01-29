55dca21e21ab46f1717a9ac80a7837f2a9e76e69
SlayBrainzZ <136199883+SlayBrainzZ@users.noreply.github.com>
2024-01-29 14:30:49 +0100

    Update LandingPage.java

--- src/App/UI/LandingPage.java
+++ src/App/UI/LandingPage.java
@@ -5,31 +5,29 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class LandingPage {
     private JFrame frame;
     private JLabel backgroundLabel;
     private JLabel titleLabel;
     private JLabel descriptionLabel;
-    private JButton connectButton;
+    //private JButton connectButton;
     private JButton startAppButton; // New button for starting the application
-
-    private JLabel connectionLabel;
-    private JLabel connectionStatusCircle;
 
     public API apiService;
 
-    public LandingPage() {
+    public LandingPage() throws MalformedURLException {
         apiService = new API(); //  instance of ApiService
         initialize();
     }
 
-    private void initialize() {
+    private void initialize() throws MalformedURLException {
         frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // Load background image and set it as the content pane
-        ImageIcon backgroundIcon = new ImageIcon("C:/Users/ahmed/Downloads/worldmap3.png");
+        ImageIcon backgroundIcon = new ImageIcon(new URL("https://i.imgur.com/QGkGoMH.png"));
         backgroundLabel = new JLabel(backgroundIcon);
         frame.setContentPane(backgroundLabel);
 
@@ -37,22 +35,6 @@ public class LandingPage {
         frame.setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.insets = new Insets(10, 10, 10, 10); // padding
-
-        // Connection label and status circle
-        connectionLabel = new JLabel("Connection");
-        connectionLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
-        connectionLabel.setForeground(Color.RED); // Initial color is red
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        gbc.anchor = GridBagConstraints.EAST;
-        frame.add(connectionLabel, gbc);
-
-        connectionStatusCircle = new JLabel("●");
-        connectionStatusCircle.setFont(new Font("Tahoma", Font.BOLD, 20));
-        connectionStatusCircle.setForeground(Color.RED); // Initial color is red
-        gbc.gridx = 1;
-        gbc.anchor = GridBagConstraints.WEST;
-        frame.add(connectionStatusCircle, gbc);
 
         // Title label
         titleLabel = new JLabel("Drone Simulator");
@@ -67,21 +49,13 @@ public class LandingPage {
 
         // Description label
         descriptionLabel = new JLabel("Welcome to the Drone Simulator application.");
-        descriptionLabel.setFont(new Font("Tahoma", Font.PLAIN, 20)); // Adjust font size
+        descriptionLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
         descriptionLabel.setHorizontalAlignment(SwingConstants.CENTER);
         descriptionLabel.setForeground(Color.WHITE);
         gbc.gridy = 2;
         gbc.gridx = 0;
         gbc.gridwidth = 2; // Span two columns
         frame.add(descriptionLabel, gbc);
-
-        // Connect button
-        connectButton = new JButton("Test API");
-        connectButton.setFont(new Font("Tahoma", Font.PLAIN, 15)); // Adjust font size
-        connectButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                // Call the DroneApiService to simulate a successful connection
-                simulateSuccessfulConnection();}});
 
         // Start Application button
         startAppButton = new JButton("Start Application");
@@ -102,7 +76,6 @@ public class LandingPage {
         gbc.gridy = 3;
         gbc.gridwidth = 2; // Span two columns
         gbc.anchor = GridBagConstraints.CENTER; // Center the connect button
-        frame.add(connectButton, gbc);
         gbc.gridy = 6;
         frame.add(startAppButton,gbc);
 
@@ -113,27 +86,8 @@ public class LandingPage {
         frame.setSize(backgroundIcon.getIconWidth(), backgroundIcon.getIconHeight());
     }
 
-    private void simulateSuccessfulConnection() {
-        try {
-            apiService.ApiRequest("drones/?format=json&limit=1000");
-            connectionLabel.setForeground(Color.GREEN); // Update connection status label to green in case of successful connection
-            connectionStatusCircle.setForeground(Color.GREEN);
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            JOptionPane.showMessageDialog(frame, "Error connecting to API: " + ex.getMessage());
-        }
-    }
-
     public void show() {
         frame.setVisible(true);
     }
 
-    public static void main(String[] args) {
-        try {
-            LandingPage window = new LandingPage();
-            window.show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 }
